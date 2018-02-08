f3102f23b42d827ae53a2d49e39de905ebb5b3ea
Matskevich Yan <macyanwork@gmail.com>
2018-02-08 23:29:28 +0300

    Added settings.local.php

---
+++ settings.local.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Settings file for local development environments.
+ *
+ * Use this to store local database credentials, API keys, and debugging
+ * settings. This file should never be committed to version control.
+ */
+
+$brand_name = 'eonline';// <--- This should match the folder name under sites
+
+$conf['pub_site_shortname'] = $brand_name;
+
+ini_set('error_reporting', E_ALL);
+ini_set('memory_limit','512M');
+
+// Define the current environment.
+$_ENV['AH_SITE_ENVIRONMENT'] = 'local';
+
+$conf['file_private_path'] = "sites/$brand_name/files/files-private";
+$conf['file_temporary_path'] = "sites/$brand_name/files/tmp";
+
+$conf['aws_sqs_aws_key'] = '';
+$conf['aws_sqs_aws_secret'] = '';
+$conf['content_registry_api_key'] = '';
+$conf['content_registry_environment_url'] = '';
+
+$databases['default'] = [
+  'default' =>
+    [
+      'database' => 'eonline',
+      'username' => 'root',
+      'password' => 'password',
+      'host' => 'mariadb',
+      'port' => '3306',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ],
+];
